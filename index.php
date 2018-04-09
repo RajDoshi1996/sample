e624b545974cb2104f35136c9264362457a22129
Raj Doshi <rajdoshi31@gmail.com>
2018-04-09 14:03:58 +0530

    MY FIRST COMMIT

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+
+<script>
+
+function z()
+{
+
+
+
+	var a=document.getElementById("num1").value;
+
+	var b=document.getElementById("num2").value;
+
+
+		if(a=="" || b=="")
+		{
+			alert("Enter both values");
+			return;
+		}
+
+	document.getElementById("para").innerHTML="sum is "+(parseInt(a)+parseInt(b));
+
+}
+</script>
+
+<body>
+	
+<input type="number" name="" id="num1">
+<br>
+<br>
+
+<input type="number" name="" id="num2">
+<br><br>
+
+<input type="button" value="Sum" onClick="z()">
+
+<br><br>
+
+<p id="para"></p>
+
+</body>
+</html>
